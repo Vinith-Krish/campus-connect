f5f6a2cdbd9de9cab147c96ef3cba60e8e84d637
Vinith-Krish <vinithkrishna20@gmail.com>
2026-04-13 10:59:09 +0530

    few changes made

--- campus-connect-backend/src/main/java/com/campusconnect/dto/LoginRequest.java
+++ campus-connect-backend/src/main/java/com/campusconnect/dto/LoginRequest.java
@@ -11,8 +11,6 @@ public class LoginRequest {
     @NotBlank(message = "Password is required")
     @Size(min = 6, message = "Password must be at least 6 characters")
     private String password;
-    @NotBlank(message = "reCAPTCHA token is required")
-    private String recaptchaToken;
 
     public LoginRequest() {}
 
@@ -20,6 +18,4 @@ public class LoginRequest {
     public void setEmail(String email) { this.email = email; }
     public String getPassword() { return password; }
     public void setPassword(String password) { this.password = password; }
-    public String getRecaptchaToken() { return recaptchaToken; }
-    public void setRecaptchaToken(String recaptchaToken) { this.recaptchaToken = recaptchaToken; }
 }
